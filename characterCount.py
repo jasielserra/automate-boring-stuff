dbf52f72b2243e6020e1b8075f546ef12c24bcef
Jasiel Serra <jasiel_serra@hotmail.com>
2016-04-21 21:21:27 -0300

    Utilizando a função pprint

--- characterCount.py
+++ characterCount.py
@@ -1,11 +1,9 @@
+import pprint
 message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
 count = {}
 
 for character in message:
-    #if character not in count:
-    #    count[character] = 1
-    #else:
     count.setdefault(character,0)
     count[character] = count[character] + 1
 
-print(count)
+pprint.pprint(count)
